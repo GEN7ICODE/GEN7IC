f5fa810e6846d87d4ac05f899d9398aed8d46a5f
HUMEND <eduardermakov769@gmail.com>
2025-11-03 02:18:38 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        POINTX
+import os        HUMEND
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
